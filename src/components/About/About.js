ffc9b25544709ee9b67b824ea21359377665bd05
Polin Rahman <61315426+Polin-Rahman@users.noreply.github.com>
2021-10-05 22:50:56 +0600

    FINAL

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,19 +1,56 @@
 import React from 'react';
 import './About.css';
 import aboutImg from '../../images/about.jpg';
+import oneImg from '../../images/one.jpg';
+import twoImg from '../../images/two.jpg';
+import threeImg from '../../images/three.jpg';
+
+import { Card, CardGroup } from 'react-bootstrap';
 
 const About = () => {
     return (
         //about us component
-        <div className="about-containar">
-            <div>
-                <img src={aboutImg} alt="" />
-            </div>
-            <div className="about-text">
-                <h1>About Us</h1>
-                <div className="about-text-details">
-                    <p>In today’s modern age of disruption, Skilled-Self Online is your ideal learning platform that enables you to upskill to the most in-demand technology skills like Microsoft Excel,Microsoft PowerPoint,Microsoft Word,Adobe Illustrator,Adobe Photoshop,Adobe XD,Personal grooming,Public Speaking,Financial Management,Time Management and many more. In your journey of evolution as a technologist, Skilled-Self Online helps you work smarter, get to your career goals faster and create an exciting technology led future.</p>
+        <div>
+            <div className="about-containar">
+                <div>
+                    <img src={aboutImg} alt="" />
                 </div>
+                <div className="about-text">
+                    <h1>About Us</h1>
+                    <div className="about-text-details">
+                        <p>In today’s modern age of disruption, Skilled-Self Online is your ideal learning platform that enables you to upskill to the most in-demand technology skills like Microsoft Excel,Microsoft PowerPoint,Microsoft Word,Adobe Illustrator,Adobe Photoshop,Adobe XD,Personal grooming,Public Speaking,Financial Management,Time Management and many more. In your journey of evolution as a technologist, Skilled-Self Online helps you work smarter, get to your career goals faster and create an exciting technology led future.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="container my-5">
+                <h2 className="text-center my-5">Our top Instuctors</h2>
+
+                <CardGroup>
+                    <Card className="p-5">
+                        <Card.Img variant="top" src={oneImg} />
+                        <Card.Body>
+                            <Card.Title>Olivia Emma</Card.Title>
+                            <p>Microsoft Office - expert</p>
+                            <small className="text-muted">Joined 1 year ago</small>
+                        </Card.Body>
+                    </Card>
+                    <Card className="p-5">
+                        <Card.Img variant="top" src={twoImg} />
+                        <Card.Body>
+                            <Card.Title>James Benjamin</Card.Title>
+                            <p>Graphic designing - expart</p>
+                            <small className="text-muted">Joined 8 months ago</small>
+                        </Card.Body>
+                    </Card>
+                    <Card className="p-5">
+                        <Card.Img variant="top" src={threeImg} />
+                        <Card.Body>
+                            <Card.Title>Alexander Henry</Card.Title>
+                            <p>Personal development - expart</p>
+                            <small className="text-muted">Joined 1.2 year ago</small>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
             </div>
 
         </div>
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,25 +5,99 @@ import contactImg from '../../images/contact.jpg';
 const Contact = () => {
     //contact component
     return (
-        <div className="contact-containar">
-            <div>
-                <img src={contactImg} alt="" />
+        <div >
+            <div className="contact-containar">
+                <div>
+                    <img src={contactImg} alt="" />
+                </div>
+                <div className="contact-steps-texts">
+
+                    <div className="contact-steps">
+                        <h2>Join with Us</h2>
+                        <p><i class="fas fa-arrow-right"></i> Create Account</p>
+                        <p><i class="fas fa-arrow-right"></i> Join Membership</p>
+                        <p><i class="fas fa-arrow-right"></i> Start Learning</p>
+                        <p><i class="fas fa-arrow-right"></i> Get Certificate</p>
+                    </div>
+
+                    <div className="contact-steps">
+                        <h2>Contact with Us</h2>
+                        <p className="text-center"><i class="fas fa-phone-alt"></i> Call : + 000 123 66 99</p>
+                        <p className="text-center"><i class="fas fa-at"></i> Email :  skilled.self@hmail.com</p>
+                    </div>
+                </div>
             </div>
-            <div className="contact-steps-texts">
 
-                <div className="contact-steps">
-                    <h2>Join with Us</h2>
-                    <p><i class="fas fa-arrow-right"></i> Create Account</p>
-                    <p><i class="fas fa-arrow-right"></i> Join Membership</p>
-                    <p><i class="fas fa-arrow-right"></i> Start Learning</p>
-                    <p><i class="fas fa-arrow-right"></i> Get Certificate</p>
-                </div>
+            <div className="w-50 mx-auto my-5">
+                <h3 className="text-center my-5">Create your account</h3>
 
-                <div className="contact-steps">
-                    <h2>Contact with Us</h2>
-                    <p className="text-center"><i class="fas fa-phone-alt"></i> Call : + 000 123 66 99</p>
-                    <p className="text-center"><i class="fas fa-at"></i> Email :  skilled.self@hmail.com</p>
-                </div>
+                <form className="row g-3 needs-validation" novalidate>
+                    <div className="col-md-4">
+                        <label for="validationCustom01" className="form-label">First name</label>
+                        <input type="text" className="form-control" id="validationCustom01" required />
+                        <div className="valid-feedback">
+                            Looks good!
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <label for="validationCustom02" className="form-label">Last name</label>
+                        <input type="text" class="form-control" id="validationCustom02" required />
+                        <div className="valid-feedback">
+                            Looks good!
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <label for="validationCustomUsername" className="form-label">Email</label>
+                        <div className="input-group has-validation">
+                            <span className="input-group-text" id="inputGroupPrepend">@</span>
+                            <input type="text" className="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required />
+                            <div className="invalid-feedback">
+                                Please choose a username.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <label for="validationCustom03" className="form-label">Address</label>
+                        <input type="text" className="form-control" id="validationCustom03" required />
+                        <div className="invalid-feedback">
+                            Please provide a valid city.
+                        </div>
+                    </div>
+                    <div className="col-md-3">
+                        <label for="validationCustom04" className="form-label">Select course</label>
+                        <select className="form-select" id="validationCustom04" required>
+                            <option selected disabled value="">Courses</option>
+                            <option>Microsoft Excel</option>
+                            <option>Adobe Illustrator</option>
+                            <option>Public Speaking</option>
+                            <option>Time Management</option>
+                        </select>
+                        <div className="invalid-feedback">
+                            Please select a valid state.
+                        </div>
+                    </div>
+                    <div className="col-md-3">
+                        <label for="validationCustom05" className="form-label">Comments</label>
+                        <input type="text" className="form-control" id="validationCustom05" required />
+                        <div className="invalid-feedback">
+                            Please provide a valid zip.
+                        </div>
+                    </div>
+                    <div className="col-12">
+                        <div className="form-check">
+                            <input className="form-check-input" type="checkbox" value="" id="invalidCheck" required />
+                            <label className="form-check-label" for="invalidCheck">
+                                Agree to terms and conditions
+                            </label>
+                            <div className="invalid-feedback">
+                                You must agree before submitting.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-12">
+                        <button className="btn btn-primary">Submit</button>
+                    </div>
+                </form>
             </div>
         </div >
     );
